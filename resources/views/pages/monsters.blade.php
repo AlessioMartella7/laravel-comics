8f098899f9c1492d7858ed9f0a1619ed849e09c6
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-23 16:35:22 +0200

    exercise complete

--- resources/views/pages/monsters.blade.php
+++ resources/views/pages/monsters.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-3 py-4">
 
                     <a href="{{ route('pages-show', $index) }}">
-                        <div class="card h-100 pb-0 bg-black rounded p-1" style="width: 18rem;">
+                        <div class="card h-100 pb-0 bg-black rounded p-1">
                             <img class="card-img-top h-50 img-fluid rounded " src="{{ $monster['image'] }}"
                                 alt="{{ $monster['name'] }}">
                             <div class="card-body bg-dark text-white rounded my-1">
